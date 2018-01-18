f7b68d51b5f72a5c7c2a5dbe357c1125234156ed
iscc7 <30551381+iscc7@users.noreply.github.com>
2018-01-18 09:01:44 +0800

    Update new_tex.py

--- new_tex.py
+++ new_tex.py
@@ -5,7 +5,7 @@ from re import compile
 
 def get_text(url):
     header = {
-        'Host': 'www.xicidaili.com',
+        'Host': 'www.kuaidaili.com',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0'
     }
     txt = get(url, headers=header)
